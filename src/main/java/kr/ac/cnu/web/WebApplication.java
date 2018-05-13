5bd01932449db17aa1edcfa7f91ce3d89cc3c906
rokim <rokim@riotgames.com>
2018-05-13 15:48:18 +0900

    Add homework week1

--- src/main/java/kr/ac/cnu/web/WebApplication.java
+++ src/main/java/kr/ac/cnu/web/WebApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @Slf4j
 public class WebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
