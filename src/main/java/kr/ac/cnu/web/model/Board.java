66a2d7a9231ac3521319b4c96c22a203614d076d
rokim <rokim@riotgames.com>
2018-05-14 17:21:38 +0900

    Add sample sql

--- src/main/java/kr/ac/cnu/web/model/Board.java
+++ src/main/java/kr/ac/cnu/web/model/Board.java
@@ -4,13 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by rokim on 2018. 5. 14..
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String content;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into board(title, content, writer_id)
+VALUES (
+  '제목입니다.1', '내용입니다.', 'ian'
+);
+insert into board(title, content, writer_id)
+VALUES (
+  '제목입니다.2', '내용입니다.', 'robin'
+);
+insert into board(title, content, writer_id)
+VALUES (
+  '제목입니다.3', '내용입니다.', 'ian'
+);
