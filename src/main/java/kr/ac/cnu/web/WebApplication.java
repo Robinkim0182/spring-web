25b7ebb273015ba8858f30fc8bfc9ee319579960
rokim <rokim@riotgames.com>
2018-05-28 14:01:21 +0900

    Add custom datasource config

--- src/main/java/kr/ac/cnu/web/WebApplication.java
+++ src/main/java/kr/ac/cnu/web/WebApplication.java
@@ -2,12 +2,15 @@ package kr.ac.cnu.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @Slf4j
 @EnableAspectJAutoProxy
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class WebApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
---
+++ src/main/java/kr/ac/cnu/web/config/DatasourceConfig.java
@@ -0,0 +1,35 @@
+package kr.ac.cnu.web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by rokim on 2018. 5. 28..
+ */
+//@Configuration
+public class DatasourceConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+
+    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
+}
